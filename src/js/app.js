430d0c17e8d6a09f8d5511f0f1ca7ddc842ca282
Federico Carrilao <federico.carrilao@gba.gob.ar>
2019-08-02 13:19:57 -0300

    cambio color

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       },
       "color": {
-        "value": "#ffffff"
+        "value": "#971212"
       },
       "shape": {
         "type": "polygon",
